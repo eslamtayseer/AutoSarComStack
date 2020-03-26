35ed72f9ef4ca298902d3883afde294430da606f
eslamtayseer <eslam_tayseer@yahoo.com>
2020-03-26 20:48:47 +0200

    Implementation of configuratoin of Can interface

---
+++ CanIf_Cfg.c
@@ -0,0 +1,42 @@
+#include "CanIf_Cfg.h"
+
+// CanIfPrivateCfg PrivateCfg []={
+
+// };
+
+CanIfRxPduCfg  RxPduCfg[] = { {
+ .CanIfRxPduCanId = 0,
+ .CanIfRxPduCanIdType = STANDARD_CAN,
+ .CanIfRxPduDlc = TRUE,
+ // CanIfHrhCfg               *CanIfRxPduHrhIdRef;
+ .CanIfRxPduId = 0,
+ .CanIfRxPduReadNotifyStatus = FALSE,
+ .CanIfRxPduReadData = FALSE,
+ .CanIfRxPduUserRxIndicationUL = PDUR
+ }
+};
+ CanIfTxPduCfg  TxPduCfg[]={
+	 {
+  .CanIfTxPduCanId = 0,
+  .CanIfTxPduCanIdType = STANDARD_CAN,
+  .CanIfTxPduId = 0,
+  .CanIfTxPduReadNotifyStatus = FALSE,
+  .CanIfTxPduTriggerTransmit = FALSE,
+  .CanIfTxPduType = STATIC,
+  .CanIfTxPduUserConfirmationUL = PDUR
+  }
+};
+ CanIfInitCfg initCfg[] = { {
+   .CanIfMaxBufferSize = 2,
+   .CanIfMaxRxPduCfg = 1,
+   .CanIfMaxTxPduCfg = 1,
+
+   // CanIfBufferCfg            *CanIfBuffersCfgs;
+   // CanIfInitHohCfg           *CanIfDrvCfgs;
+   .CanIfRxPdusCfgs = &RxPduCfg[0],
+   .CanIfTxPdusCfgs = &TxPduCfg[0]
+   }
+};
+const CanIf_ConfigType configuration ={
+    .InitConfig =&initCfg[0]
+};
--- CanIf_Cfg.h
+++ CanIf_Cfg.h
@@ -15,16 +15,16 @@ typedef struct
   uint32                     CanIfRxPduCanId;
   CanIfPduCanIdType          CanIfRxPduCanIdType;
   uint8                      CanIfRxPduDlc;
-  CanIfHrhCfg               *CanIfRxPduHrhIdRef;
+  // CanIfHrhCfg               *CanIfRxPduHrhIdRef;
   uint32                     CanIfRxPduId;
   boolean                    CanIfRxPduReadNotifyStatus;
   boolean                    CanIfRxPduReadData;
-  CanIfPduUserConfirmationUL CanIfRxPduUserConfirmationUL;
+  CanIfPduUserConfirmationUL CanIfRxPduUserRxIndicationUL;
 } CanIfRxPduCfg;
 
 typedef struct
 {
-  CanIfBufferCfg            *CanIfTxPduBufferRef;
+  // CanIfBufferCfg            *CanIfTxPduBufferRef;
   uint32                     CanIfTxPduCanId;
   CanIfPduCanIdType          CanIfTxPduCanIdType;
   uint32                     CanIfTxPduId;
@@ -40,11 +40,21 @@ typedef struct
   uint64                     CanIfMaxRxPduCfg;
   uint64                     CanIfMaxTxPduCfg;
   
-  CanIfBufferCfg            *CanIfBuffersCfgs;
-  CanIfInitHohCfg           *CanIfDrvCfgs;
+  // CanIfBufferCfg            *CanIfBuffersCfgs;
+  // CanIfInitHohCfg           *CanIfDrvCfgs;
   CanIfRxPduCfg             *CanIfRxPdusCfgs;
   CanIfTxPduCfg             *CanIfTxPdusCfgs;
 } CanIfInitCfg;
+
+typedef struct {
+  	// CanIfCtrlCfg* ControllerConfig;
+
+	
+	CanIfInitCfg* InitConfig;
+
+}CanIf_ConfigType;
+
+extern const  CanIf_ConfigType configuration;
 
 #endif
 
